9f99e30d03a3fc5d61ad50311e68f839a4d8be6b
AlianGrey <ek371116@gmail.com>
2023-07-01 18:14:40 +0400

    show modal window by the time period & then a user has scrolled to the end of the page

--- dist/js/script.js
+++ dist/js/script.js
@@ -112,33 +112,50 @@ window.addEventListener('DOMContentLoaded', () => {
         modal = document.querySelector('.modal');
 
     function closeModal() {
-        /*     modal.classList.remove('show', 'fade');
-            modal.classList.add('hide'); */
+        /*modal.classList.remove('show', 'fade');
+        modal.classList.add('hide'); */
         modal.classList.toggle('show');
         document.body.style.overflow = '';
     }
 
-    //modalTrigger[0].addEventListener('click', openModalWindow);
+    function openModal() {
+        /*modal.classList.remove('hide');
+        modal.classList.add('show', 'fade'); */
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        //console.log(modal.style.display = 'block');
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            /*modal.classList.remove('hide');
-            modal.classList.add('show', 'fade'); */
-            modal.classList.toggle('show');
-            document.body.style.overflow = 'hidden';
-            //console.log(modal.style.display = 'block');
-        })
+        btn.addEventListener('click', openModal)
     });
 
     modalCloseBtn.addEventListener('click', closeModal);
 
+    //close modal window then click outside the modal window
     modal.addEventListener('click', (e) => {
         if (e.target == modal) { closeModal(); }
     });
 
+    //close modal window then down "Esc" key
     document.addEventListener('keydown', (e) => {
-        if (e.code == 'Escape' && modal.classList.contains('show')) {
-            closeModal();
-        }
+        if (e.code == 'Escape' && modal.classList.contains('show')) { closeModal(); }
     });
+
+    //show modal window then past 3000ms from open the site if before it was not open
+    const modalTimerId = setTimeout(openModal, 50000);
+
+    //show the modal window if a user has scrolled to the bottom of the page
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        };
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+
 
 });
