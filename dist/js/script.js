5cc1146707a16cbcb20020e208d5c065d50db0ce
AlianGrey <ek371116@gmail.com>
2023-07-09 10:34:42 +0400

    use axios library for post element to the json file

--- dist/js/script.js
+++ dist/js/script.js
@@ -200,33 +200,50 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     const elem = '.menu .container';
 
-    new MenuCard(
-        "img/tabs/vegy.jpg",
-        'vegy',
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        elem,
-        'menu__item', 'big'
-    ).render();
-    new MenuCard(
-        "img/tabs/elite.jpg",
-        'elite',
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        elem,
-        'menu__item'
-    ).render();
-    new MenuCard(
-        "img/tabs/post.jpg",
-        'post',
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        elem,
-        'menu__item',
-    ).render();
+    const getResource = async (url) => {
+        const res = await fetch(url);
+        if (!res.ok) { //тк catch не считает 400, 500 за ошибки и игнорирует их, это позволит перебросить ошибку в блок catch
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    };
+
+    /*     getResource('http://localhost:3000/menu')
+            .then((data) => {
+                data.forEach(({ img, altimg, title, descr, price }) => {
+                    new MenuCard(img, altimg, title, descr, price, elem).render();
+                });
+            }); */
+
+    //varian throw axios library
+    axios.get('http://localhost:3000/menu')
+        .then((data) => {
+            data.data.forEach(({ img, altimg, title, descr, price }) => {
+                new MenuCard(img, altimg, title, descr, price, elem).render();
+            });
+        });
+
+    // 2 variant
+    /*     getResource('http://localhost:3000/menu')
+            .then((data) => createCard(data));
+    
+        function createCard(data) {
+            data.forEach(({ img, altimg, title, descr, price }) => {
+                const element = document.createElement('div');
+                element.classList.add('menu__item');
+                element.innerHTML = `
+                    <img src=${img} alt=${altimg}>
+                    <h3 class="menu__item-subtitle">${title}</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> дол/день</div>
+                    </div>
+                `;
+                document.querySelector(elem).append(element);
+            });
+        } */
 
     //Forms
     const forms = document.querySelectorAll('form');
@@ -238,10 +255,21 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     forms.forEach(item => {
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: data
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -253,23 +281,12 @@ window.addEventListener('DOMContentLoaded', () => {
              `;
 
             form.insertAdjacentElement('afterend', statusMessage); // в конец формы после закрытия тега
-            //form.append(statusMessage); //в конец формы перед закрытием тега
 
             const formData = new FormData(form); //формируем данные с формы для отправки на сервер, надо чтобы name у всех элементов был
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-                .then(data => data.text())
+            postData('http://localhost:3000/requests', json)
                 .then(data => {
                     console.log(data);
                     showThanksModal(message.success);
@@ -303,16 +320,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
         setTimeout(() => {
             thanksModal.remove();
-
             prevModalDialog.classList.add('show');
-
             prevModalDialog.classList.remove('hide');
-
             closeModal();
-
         }, 4000);
-
     }
-
 
 });
